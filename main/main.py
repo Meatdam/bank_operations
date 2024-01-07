15aa25cbe33265c8be9bfbbc684df741cdcb0c13
kuzkin Ilya <heartsong_@mail.ru>
2024-01-07 21:32:35 +0500

    fix main.py

--- main/main.py
+++ main/main.py
@@ -1,4 +1,12 @@
-from src.utils import get_date_format
+from src.utils import *
 
+load_json = load_json_operations()
+sort_list = sort_list_operations(load_json)
+format_date = format_date_operations(sort_list)
+format_check = get_format_check(sort_list)
+format_card = get_format_card(sort_list)
 
-print(get_date_format())  # вызываем функцию последних 5 операций
+for operation in range(len(sort_list)):
+    print(f"{format_date[operation]} {sort_list[operation]['description']}\n{format_card[operation]} --> "
+          f"{format_check[operation]}\n{sort_list[operation]['operationAmount']['amount']} "
+          f"{sort_list[operation]['operationAmount']['currency']['name']}\n")
